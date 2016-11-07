584f67a9782756b4ccbb38bb662fb3f0beb2676a
chengyurou <18829292695@163.com>
2016-11-07 06:20:08 -0500

    finish save post comments

--- public/db/db-posts.js
+++ public/db/db-posts.js
@@ -1,19 +1,17 @@
 var url = "mongodb://localhost:27017/nodebb";
 var MongoClient = require('mongodb').MongoClient;
 
-exports.saveComment = function (comment, callback) {
-
-	console.log(comment.pid);
+exports.saveComment = function (comment,com_id, callback) {
 
 	MongoClient.connect(url, function (err, db) {
 
 		var collection = db.collection('objects');
 
-		collection.update({"pid": 32},
+		collection.update({"pid": parseInt(comment.pid)},
 			{
 				$push: {
 					"comments": {
-						"com_id": 10,
+						"com_id": com_id,
 						"com_content": comment.com_content
 					}
 				}
